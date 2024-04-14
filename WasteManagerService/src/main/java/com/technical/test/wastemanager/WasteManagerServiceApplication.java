292c46ccffe018651d22e516b2df697f96854d4b
hgrodriguez91 <humbertog@arqs.co>
2024-04-14 11:23:31 -0400

    Configuring gateway service

--- WasteManagerService/src/main/java/com/technical/test/wastemanager/WasteManagerServiceApplication.java
+++ WasteManagerService/src/main/java/com/technical/test/wastemanager/WasteManagerServiceApplication.java
@@ -3,6 +3,7 @@ package com.technical.test.wastemanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WasteManagerServiceApplication {
 
